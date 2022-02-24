6dcdc8e33f787764e7b8c5a9d1bdd84f3aa0e460
Viktor Hopkins <viktorhopkins00@gmail.com>
2022-02-24 21:50:21 +0100

    Single Ticket Display

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -28,9 +28,23 @@ const getTickets = async (token) => {
     return response.data
 }
 
+// Get user ticket
+const getTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization : `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.post(API_URL + ticketId, config)
+
+    return response.data
+}
+
 const ticketService = {
     createTicket,
-    getTickets
+    getTickets,
+    getTicket
 }
 
 export default ticketService
